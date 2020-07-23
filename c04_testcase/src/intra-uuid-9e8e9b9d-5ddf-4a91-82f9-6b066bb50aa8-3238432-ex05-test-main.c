68d174c7ee2bf668b70fb7878b8c52bffd0d9836
sooh <01moses@naver.com>
2020-07-23 21:59:22 +0900

    ??

---
+++ c04_testcase/src/intra-uuid-9e8e9b9d-5ddf-4a91-82f9-6b066bb50aa8-3238432-ex05-test-main.c
@@ -0,0 +1,48 @@
+
+	#include <stdio.h>
+	#include <stdarg.h>
+	#include <string.h>
+	#include <stdlib.h>
+
+
+	int		ft_atoi_base(char *str, char *base);
+
+	void printmsg(const char *msg, ...)
+	{
+		char *newstr = (char *)malloc(strlen(msg) + 2);
+		strcpy(newstr, msg);
+		strcat(newstr, "\n");
+
+		va_list args;
+		va_start(args, msg);
+		vprintf(newstr, args);
+		va_end(args);
+		// printf("%s\n", msg);
+		free(newstr);
+	}
+
+	void printline()
+	{
+		printf("\n------------------------------------------------------------\n");
+	}
+
+	void print_testing(const char *code)
+	{
+		printmsg("\n## testing code :");
+		printf("%s", code);
+	}
+
+	int main()
+	{
+		
+	printf("%d ", ft_atoi_base("42", "0123456789"));
+	printf("%d ", ft_atoi_base("   42", "0123456789"));
+	printf("%d ", ft_atoi_base("  +--++-rcrdmddd+-abc", "mrdoc"));
+	printf("%d ", ft_atoi_base("-10000000000000000000000000000000", "01"));
+	printf("%d ", ft_atoi_base("8888888888888888888888888888888", "a8"));
+	printf("%d ", ft_atoi_base("one", "e"));
+	printf("%d ", ft_atoi_base("blank", ""));
+	printf("\n");
+	}
+
+	
